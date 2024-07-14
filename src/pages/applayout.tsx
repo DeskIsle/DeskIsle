3f3b25ddc83b051f4da5b9ff225ee85dd2d1e024
KeyonYan <835254227@qq.com>
2024-07-14 23:11:15 +0800

    🐞 fix: update comp potion while dragTransitionEnd

--- src/pages/applayout.tsx
+++ src/pages/applayout.tsx
@@ -7,23 +7,16 @@ import { Comp, isDraggingAtom, registryComps, splitCompAtoms } from "@/atoms/com
 
 export const AppLayout = () => {
   const [compAtoms] = useAtom(splitCompAtoms)
-  const [{unit, gap}] = useAtom(layoutConfigAtom)
+  const [{ unit, gap }] = useAtom(layoutConfigAtom)
   const layoutRef = useRef<HTMLDivElement>(null)
-  const [, setIsDragging] = useAtom(isDraggingAtom)
   const [layoutConfig] = useAtom(layoutConfigAtom)
-  function handleDragStart() {
-    setIsDragging(true)
-  }
-  function handleDragEnd() {
-    setIsDragging(false)  
-  }
 
   const modifyTargetForGridLayout = (target: number) => {
     return Math.round(target / (unit + gap)) * (unit + gap)
   }
   return (
-    <div 
-      ref={layoutRef} 
+    <div
+      ref={layoutRef}
       className="relative w-full h-full"
     >
       {compAtoms.map((atom, index) => {
@@ -37,8 +30,6 @@ export const AppLayout = () => {
               modifyTarget: target => modifyTargetForGridLayout(target)
             }}
             dragConstraints={layoutRef}
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
           />
         )
       })}
@@ -53,28 +44,57 @@ interface CompProps extends MotionProps {
   className?: string,
 }
 
-export function CompElement({compAtom, className, ...props}: CompProps) {
-  const [comp] = useAtom(compAtom)
-  const { element, width, height, row, col} = comp
-  const [{unit, gap}] = useAtom(layoutConfigAtom)
+export function CompElement({ compAtom, className, ...props }: CompProps) {
+  const [comp, setComp] = useAtom(compAtom)
+  const ref = useRef<HTMLDivElement>(null)
+  const { element, width, height, row, col } = comp
+  const [{ unit, gap }] = useAtom(layoutConfigAtom)
   const Element = registryComps[element].Element
-  
+  const [, setIsDragging] = useAtom(isDraggingAtom)
+  function handleDragStart() {
+    setIsDragging(true)
+  }
+  function handleDragEnd() {
+    setIsDragging(false)
+  }
+
+  function updateCompPosition() {
+    const targetElement = ref.current
+    if (targetElement) {
+      const reg = /\d+(\.\d+)?/g
+      const matchRes = targetElement.style.transform.match(reg)
+      const [x, y] = matchRes ?? [0, 0]
+      const newRow = Math.floor(Number(y) / (unit + gap))
+      const newCol = Math.floor(Number(x) / (unit + gap))
+      console.log(newRow, newCol)
+      setComp({
+        ...comp,
+        row: newRow,
+        col: newCol
+      })
+    }
+  }
+
   return (
     <motion.div
+      ref={ref}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragTransitionEnd={updateCompPosition}
       initial={{
         x: 0,
-        y: 0, 
+        y: 0,
       }}
       animate={{
-        x: (unit+gap)*col,
-        y: (unit+gap)*row,
-        width: unit*width+((width-1)*gap), 
-        height: unit*height+((height-1)*gap),
+        x: (unit + gap) * col,
+        y: (unit + gap) * row,
+        width: unit * width + ((width - 1) * gap),
+        height: unit * height + ((height - 1) * gap),
       }}
       className={cn(className, `absolute bg-transparent rounded-lg shadow-sm flex justify-center items-center overflow-hidden`)}
       {...props}
-      >
-      <Element compAtom={compAtom}/>
+    >
+      <Element compAtom={compAtom} />
     </motion.div>
   )
 }
