90ae8de49e0cee4931195639ae8b2a09928bcc57
Shadowsight9 <huangzheng@shu.edu.cn>
2024-08-31 11:17:07 +0800

    feat: update main.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,39 +1,9 @@
-import { app, shell, BrowserWindow, ipcMain } from "electron";
-import { join } from "path";
-import { electronApp, optimizer, is } from "@electron-toolkit/utils";
-import icon from "../../resources/icon.png?asset";
+import { electronApp, optimizer } from "@electron-toolkit/utils";
 
-function createWindow(): void {
-	// Create the browser window.
-	const mainWindow = new BrowserWindow({
-		width: 900,
-		height: 670,
-		show: false,
-		autoHideMenuBar: true,
-		...(process.platform === "linux" ? { icon } : {}),
-		webPreferences: {
-			preload: join(__dirname, "../preload/index.js"),
-			sandbox: false,
-		},
-	});
+import { BrowserWindow, app } from "electron";
 
-	mainWindow.on("ready-to-show", () => {
-		mainWindow.show();
-	});
-
-	mainWindow.webContents.setWindowOpenHandler((details) => {
-		shell.openExternal(details.url);
-		return { action: "deny" };
-	});
-
-	// HMR for renderer base on electron-vite cli.
-	// Load the remote URL for development or the local html file for production.
-	if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
-		mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
-	} else {
-		mainWindow.loadFile(join(__dirname, "../renderer/index.html"));
-	}
-}
+import { createTray } from "./tray";
+import { createWindow } from "./window";
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -49,26 +19,21 @@ app.whenReady().then(() => {
 		optimizer.watchWindowShortcuts(window);
 	});
 
-	// IPC test
-	ipcMain.on("ping", () => console.log("pong"));
-
-	createWindow();
+	const window = createWindow();
+	createTray(window);
 
 	app.on("activate", () => {
 		// On macOS it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
 		if (BrowserWindow.getAllWindows().length === 0) createWindow();
 	});
-});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+	// Quit when all windows are closed, except on macOS. There, it's common
+	// for applications and their menu bar to stay active until the user quits
+	// explicitly with Cmd + Q.
+	app.on("window-all-closed", () => {
+		if (process.platform !== "darwin") {
+			app.quit();
+		}
+	});
 });
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
