3c26eeb29334d55cb86d8623d8f19142f5e2ccaf
yankeyu <yankeyu@xiaomi.com>
2024-09-16 22:23:22 +0800

    build: main

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -3,21 +3,29 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		lib: {
-			entry: resolve(__dirname, "src/main.tsx"),
-			formats: ["es"],
-			fileName: "index",
-		},
-		outDir: "dist",
-		emptyOutDir: false,
-		sourcemap: true,
-		target: "esnext",
-	},
-	resolve: {
-		alias: {
-			"@": resolve(__dirname, "src"),
-		},
-	},
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+    lib: {
+      entry: resolve(__dirname, "src/main.tsx"),
+      formats: ["es"],
+      fileName: "index",
+    },
+    outDir: "dist",
+    emptyOutDir: false,
+    sourcemap: true,
+    target: "esnext",
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  define: {
+    "process.env": process.env,
+  },
 });
