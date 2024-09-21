fd4798ffcec5ce9fef46cd860f157078f3f54560
Shadowsight9 <huangzheng@shu.edu.cn>
2024-09-21 09:12:08 +0800

    chore: test

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import { ModalProvider } from "react-modal-hook";
 import { Desk } from "./pages/desk";
 
+console.log("DeskIsHere");
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<ModalProvider>
 		<Desk />
