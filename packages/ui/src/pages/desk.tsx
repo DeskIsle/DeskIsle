c5195cbbea918f17ecde0a86fc372fda81cf2ee9
KeyonYan <835254227@qq.com>
2024-10-06 22:27:02 +0800

    feat: reset atom

--- packages/ui/src/pages/desk.tsx
+++ packages/ui/src/pages/desk.tsx
@@ -2,13 +2,19 @@ import { Button } from "@/components/ui/button";
 import { useModal } from "@/components/ui/use-modal";
 import { useRef } from "react";
 
+import { componentsAtoms } from "@/atoms/components";
+import { layoutConfigAtom } from "@/atoms/layout";
 import { AppLayout } from "@/components/common/app-layout";
 import { SettingWidget } from "@/components/widgets/setting";
 import { StoreWidget } from "@/components/widgets/store";
 import { isElectron } from "@/lib/env";
+import { useAtom } from "jotai";
+import { RESET } from "jotai/utils";
 
 export function Desk() {
 	const ref = useRef(null);
+	const [, setComponents] = useAtom(componentsAtoms);
+	const [, setLayoutConfig] = useAtom(layoutConfigAtom);
 
 	const { confirm } = useModal();
 
@@ -33,6 +39,12 @@ export function Desk() {
 	window.api?.onOpenSettings(() => {
 		showSettings();
 	});
+
+	const clearStorage = () => {
+		setComponents(RESET);
+		setLayoutConfig(RESET);
+	};
+
 	return (
 		<div ref={ref} className="w-screen p-4 overflow-hidden bg-white">
 			<AppLayout parentRef={ref} />
@@ -44,6 +56,9 @@ export function Desk() {
 					</Button>
 					<Button variant="outline" className="mt-2 mr-2" onClick={showSettings}>
 						设置
+					</Button>
+					<Button variant="outline" className="mt-2 mr-2" onClick={clearStorage}>
+						重置状态管理
 					</Button>
 				</>
 			)}
